499c912900415354aaccbd521254ca1aaa74b88c
Sarfraz Khan <sksarfrazkhan76@gmail.com>
2019-12-07 22:18:20 +0530

    Added two enums TRIP_TYPE and TRIP_STATUS

--- route-menu-usermgmt/src/main/java/com/routemenu/utils/TripDirection.java
+++ route-menu-usermgmt/src/main/java/com/routemenu/utils/TripDirection.java
@@ -1,5 +1,14 @@
 package com.routemenu.utils;
 
 public class TripDirection {
+	
+	enum TRIP_TYPE {
+		ADHOC, PLANNNED 
+		}  
+	
+	enum TRIP_STATUS { 
+		PLANNNED,ALLOCATED,STARTED,COMPLETED 
+		}  
 
+	
 }
